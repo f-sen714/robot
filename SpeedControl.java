2932183f545804235aea103d506a7ad525991d4a
f-sen714 <farismarsh401@gmail.com>
2025-03-13 00:03:10 +0000

    Add files via upload

---
+++ SpeedControl.java
@@ -0,0 +1,62 @@
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.Color;
+import lejos.robotics.navigation.MovePilot;
+import lejos.hardware.sensor.SensorMode;
+import lejos.robotics.subsumption.Behavior;
+
+public class SpeedControl implements Behavior {
+	
+	private MovePilot pilot;
+	private EV3ColorSensor colorSensor;
+	private boolean suppressed = false;
+	
+    public SpeedControl(MovePilot pilot, EV3ColorSensor colorSensor) {
+        this.pilot = pilot;
+        this.colorSensor = colorSensor;
+    }
+    
+    public boolean takeControl() {
+    	SensorMode colorIDMode = colorSensor.getColorIDMode();
+		float [] sample = new float[colorIDMode.sampleSize()];
+		colorIDMode.fetchSample(sample, 0);
+		int color = (int) sample[0];
+		
+		return (color == Color.GREEN || color == Color.ORANGE); 
+    }
+	
+	public void action() {
+		suppressed = false;
+		
+		double fastSpeed = 20.0;
+		double slowSpeed = 5.0;
+		double normalSpeed = 10.0;
+		
+		SensorMode colorIDMode = colorSensor.getColorIDMode();
+		float [] sample = new float[colorIDMode.sampleSize()];
+		
+		while (!suppressed) {
+			colorIDMode.fetchSample(sample, 0);
+			int color = (int) sample[0];
+
+			if (color == Color.GREEN) {
+				pilot.setLinearSpeed(fastSpeed);
+			} else if (color == Color.ORANGE) {
+				pilot.setLinearSpeed(slowSpeed);
+			} else {
+				pilot.setLinearSpeed(normalSpeed);
+			}
+		
+			pilot.forward();
+			Thread.yield();
+
+		}
+		
+		pilot.stop();
+	
+	}
+	
+	public void suppress() {
+		suppressed = true;
+	}
+	
+}
